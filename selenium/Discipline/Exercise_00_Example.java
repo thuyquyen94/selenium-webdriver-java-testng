b08eba0e9f2f9b9f09b004362915d62eb5c9295d
Thùy Quyên <thuyquyen199400@gmail.com>
2026-01-03 21:42:06 +0700

    Restudy_Part3_Button

--- selenium/Discipline/Exercise_00_Example.java
+++ selenium/Discipline/Exercise_00_Example.java
@@ -17,7 +17,6 @@ public class Exercise_00_Example {
         driver.get("https://demo.nopcommerce.com");
     }
 
-    // 2- Action/ Execute: Tương tác lên Element nào/ nhập liệu/ verify...
     @Test
     public void TC_01_Register(){
 
@@ -27,7 +26,6 @@ public class Exercise_00_Example {
 
     }
 
-    // 3- Clean: Delete data test/ account/ close browser...
     @AfterClass
     public void cleanBrowser(){
         driver.quit();
--- selenium/Discipline/Exercise_06_Button_2.java
+++ selenium/Discipline/Exercise_06_Button_2.java
@@ -1,35 +1,62 @@
 package Discipline;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.sql.SQLOutput;
 import java.time.Duration;
+import java.util.Random;
 
 public class Exercise_06_Button_2 {
 	WebDriver driver;
+	String email, password;
+	Random rand;
 
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		rand = new Random();
+		password = "123456";
+		email = " nameAutomation" + rand.nextInt(99999) + "@gmail.com";
+
 	}
 
 	@Test
 	public void TC_01_() {
-	
+		driver.get("https://www.fahasa.com/customer/account/create");
+
+	   driver.findElement(By.cssSelector("li.popup-login-tab-login")).click();
+
+	   By loginButton = By.cssSelector("button.fhs-btn-login");
+
+	    // is Enabled: nếu element mà nó enabled thì sẽ trả về true / disable sẽ trả về false
+		// Mong đợi disable
+		Assert.assertFalse(driver.findElement(loginButton).isEnabled());
+
+		Assert.assertEquals(Color.fromString(driver.findElement(loginButton).getCssValue("background-color")).asHex().toUpperCase(),"#000000");
+
+		driver.findElement(By.cssSelector("input#login_username")).sendKeys(email);
+		driver.findElement(By.cssSelector("input#login_password")).sendKeys(password);
+		sleepInSecond(2);
+
+		Assert.assertTrue(driver.findElement(loginButton).isEnabled());
+
+		Assert.assertEquals(Color.fromString(driver.findElement(loginButton).getCssValue("background-color")).asHex().toUpperCase(), "#C92127");
+
+		driver.findElement(loginButton).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.fhs-login-msg")).getText(),"Số điện thoại/Email hoặc Mật khẩu sai!");
+
+
 	}
 
-	@Test
-	public void TC_02_() {
-		
-	}
-	@Test
-	public void TC_03_() {
-	
-	}
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
@@ -40,6 +67,6 @@ public class Exercise_06_Button_2 {
 	
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+	driver.quit();
 	}
 }
